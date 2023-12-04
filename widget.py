2233403f84e948e5db0df6416253411d010631a1
IlliaKarpenko <bigstudent24@gmail.com>
2023-12-04 23:26:43 +0100

    Initial commit with an empty UI

---
+++ widget.py
@@ -0,0 +1,15 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from ui_example import Ui_MainWindow
+
+class MainWindow(QMainWindow):
+    def __init__(self, parent=None):
+        super(MainWindow, self).__init__(parent)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
